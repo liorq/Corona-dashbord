17012a024dd580baf1f93503a46360d791540b01
lior daniel <liorqm@gmail.com>
2023-03-28 00:17:22 +0300

    rgrg

--- components/option/option.component.ts
+++ components/option/option.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-option',
   templateUrl: './option.component.html',
-  styleUrls: ['./option.component.css']
+  styleUrls: ['./option.component.css','../generic-table/generic-table.component.css',
+  '../my-options/my-options.component.css',]
 })
 export class OptionComponent {
-
+@Input()innerOption?:any;
+@Input()selectedOptions:any[]=[];
+eventStopProp(event: Event) {
+  event?.stopPropagation();
+}
 }
